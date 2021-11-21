3305d1956279ca7293ba6d147cac2c78e2ba8a74
unknown <rostyslav.hereha.ki.2021@lpnu.ua>
2021-11-21 13:59:10 +0200

    first test

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
+import logging
 import telebot
 from flask import Flask, request
 
-TOKEN = '2118961153:AAFISocvOir_rVhDEXMGHUL4NCJaaMzg4ng'
-APP_URL = f'https://git.heroku.com/freeepicgamesbot.git{TOKEN}'
+TOKEN = "2118961153:AAFISocvOir_rVhDEXMGHUL4NCJaaMzg4ng"
+APP_URL = 'https://freeepicgamesbot.herokuapp.com/'+TOKEN
 bot = telebot.TeleBot(TOKEN)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
 server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'hello,' + message.from_user)
+    bot.reply_to(message, 'hello,' + message.from_user.first_name)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
@@ -27,7 +30,6 @@ def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     return '!', 200
-
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
