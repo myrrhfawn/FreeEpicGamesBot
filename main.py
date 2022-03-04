6c4611269fb2c3f0e2e3f97a7aeaf56904618f58
rostyk <rostyslav.hereha.ki.2021@lphu.ua>
2022-03-05 00:58:18 +0200

    last ua ver

--- main.py
+++ main.py
@@ -82,5 +82,5 @@ def webhook():
 
 if __name__ == '__main__':
     print("start")
-    locale.setlocale(locale.LC_TIME, 'uk_UA.utf8')
+    locale.setlocale(locale.LC_TIME, 'ru_UA.utf8')
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
